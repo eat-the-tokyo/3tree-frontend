c49c6484821ea393a51e5908128f52cfa5c6cc70
최유진 <cuj10594@naver.com>
2023-04-15 14:36:57 +0900

    style: add font and color style

---
+++ src/utils/style/Typography/index.js
@@ -0,0 +1,19 @@
+// import { store, useGlobalState } from "state-pool";
+import React, { useState } from "react";
+import TypographyEn from "./typographyEn";
+import TypographyKo from "./typographyKo";
+
+const Typography = () => {
+  let language = localStorage.getItem("language");
+  if (!language) {
+    language = "en";
+  }
+
+  if (language === "en") {
+    return TypographyEn;
+  } else {
+    return TypographyKo;
+  }
+};
+
+export default Typography();
